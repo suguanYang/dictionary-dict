57c910e5d06017f787929ef35986a079f83c90e5
binqi.wang <binqi.wang@thoughtworks.com>
2020-08-06 19:25:24 +0800

    feat add header component

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('valorant app is running!');
+    expect(page.getTitleText()).toEqual('dict app is running!');
   });
 
   afterEach(async () => {
